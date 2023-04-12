fcd3b0444feef62739d8b379c4a099cc98a62de3
Hannes Bröhl <hannes.broehl@icloud.com>
2023-04-12 17:18:25 +0200

    Create script.js
    create script.js

---
+++ drop-down-menü/script.js
@@ -0,0 +1,5 @@
+const dropBtn = document.getElementById('dropdown')
+dropBtn.addEventListener('click', function (){
+    const open = document.querySelector('.toggle')
+    open.classList.toggle('actice')
+})
